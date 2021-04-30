08a6c5fed1a39ba9e89195e98d699988acc481db
Jegahan <83063812+Jegahan@users.noreply.github.com>
2021-04-30 13:30:39 +0000

    PVP Spielablauf
    Besseres Spacing und aussehen

--- pvp_game.py
+++ pvp_game.py
@@ -6,11 +6,11 @@ import game_functions
 def launch(player_1, player_2, board_size):
     settings.clear_screen()
 
-    print("Mensch gegen Mensch\n")
+    print("  Mensch gegen Mensch\n")
     player_1 = game_functions.set_new_player(board_size)
     player_2 = game_functions.set_new_player(board_size, player_1["name"])
     settings.clear_screen()
-    print(f"Willkommen, {player_1.get('name')} und {player_2.get('name')}!")
+    print(f"  Willkommen, {player_1.get('name')} und {player_2.get('name')}! Möge die Schlacht beginnen!\n")
 
     # Set possible input list
     possible_input = []
@@ -20,19 +20,22 @@ def launch(player_1, player_2, board_size):
 
     # Choose starting player randomly
     active_player = random.choice([player_1, player_2])
-    settings.clear_screen()
+
 
     # Starting the game
-    print("Möge die Schlacht beginnen!\n")
-    print(f'{active_player["name"]} fägt an. Drücken Sie ENTER wenn nur noch {active_player["name"]} am Computer ist:')
+    print(f'  {active_player["name"]} fägt an. Drücken Sie ENTER wenn nur noch {active_player["name"]} am Computer ist:')
     input()
     while True:
         if active_player == player_1:
             player_input = game_functions.ask_input_from(player_1, possible_input)
+            if player_input == "EXIT":
+                break;
             (player_1, player_2) = game_functions.update_boards(player_1, player_2, player_input)
             active_player = game_functions.switch_player(active_player, player_1, player_2)
 
         else:
             player_input = game_functions.ask_input_from(player_2, possible_input)
+            if player_input == "EXIT":
+                break;
             (player_2, player_1) = game_functions.update_boards(player_2, player_1, player_input)
             active_player = game_functions.switch_player(active_player, player_1, player_2)
