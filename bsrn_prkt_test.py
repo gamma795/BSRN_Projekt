4f61fb080f8ecb362bd1121c935d534e6d235d0c
gamma795 <82504110+gamma795@users.noreply.github.com>
2021-04-30 10:45:31 +0200

    Add files via upload

---
+++ bsrn_prkt_test.py
@@ -0,0 +1,26 @@
+
+
+def spieler_modus():
+    text = "Einzelspieler- (E) oder Mehrspielermodus (M) ?\nBitte 'E' oder 'M' eingeben: "
+    modus = input("Hallo")
+
+    if modus == "E":
+            # Einzelspielermodus starten
+
+
+
+
+
+
+
+# ------------------------------------------
+
+
+
+
+
+
+# ------------------------------------------
+
+
+spieler_modus()
