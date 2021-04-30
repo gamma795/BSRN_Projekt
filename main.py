72b3f1aa46ef8b591aa5694909dc40bb24058d81
Jegahan <83063812+Jegahan@users.noreply.github.com>
2021-04-30 13:28:19 +0000

    Hauptmenu
    Besseres Spacing und Reihenfolge der Anweisungen

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-
 import pve_game
 import pvp_game
 import settings
@@ -9,41 +7,61 @@ import game_functions
 board_size = 10
 player_1 = {"name": "Player 1", "score": 0}
 player_2 = {"name": "Player 2", "score": 0}
-#could import stat from existing file
+# could import statistic from existing file
+
+def show_main_menu():
+    settings.clear_screen()
+    print("  Wilkommen bei SCHIFFEVERSENKEN-2021 \n")
+    print("  Was wollen Sie tun:\n\n   1: Spiel starten \n   2: Spielfeldgröße ändern \n   3: Spiel verlassen")
+
 
 # Start menu
 settings.clear_screen()
-print("Wilkommen bei Schiffeversenken 2021 \n")
+show_main_menu()
+print("\n")
 while True:
     try:
-        main_menu_input = int(
-            input(
-                "Was wollen Sie tun:\n\n  1: Spiel starten \n  2: Spielfeldgröße ändern \n  3: Spiel verlassen \n\nIhre Wahl: "))
+        main_menu_input = int(input("  Ihre Wahl: "))
 
         if main_menu_input == 1:
             settings.clear_screen()
             number_of_human_players = settings.game_typ()
             if number_of_human_players == 1:
-                pve_game.launch(player_1,player_2, board_size)
+                pve_game.launch(player_1, player_2, board_size)
+                show_main_menu()
+                print("\n")
+                continue
 
             elif number_of_human_players == 2:
-                pvp_game.launch(player_1,player_2, board_size)
+                pvp_game.launch(player_1, player_2, board_size)
+                show_main_menu()
+                print("\n")
+                continue
 
             elif number_of_human_players == 3:
                 settings.clear_screen()
+                show_main_menu()
+                print("\n")
                 continue
 
 
         elif main_menu_input == 2:
             settings.clear_screen()
             board_size = settings.set_board_size()
+            settings.clear_screen()
+            show_main_menu()
+            print("\n")
+
 
         elif main_menu_input == 3:
             settings.clear_screen()
-            print("Auf Wiedersehen!")
+            print(" Auf Wiedersehen!")
             exit()
         else:
+            show_main_menu()
             settings.invalid_input()
     except ValueError:
+        show_main_menu()
         settings.invalid_input()
         continue
+
